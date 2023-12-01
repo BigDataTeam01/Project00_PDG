26a84b5760f306828e186e68b215376a2f3f7214
NAMINHYUCK <151398501+NAMINHYUCK@users.noreply.github.com>
2023-12-01 19:04:41 +0900

    Quiz_01
    10*^

---
+++ Quiz.java
@@ -0,0 +1,18 @@
+package com.javalec.base;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		// 10의 0제곱 ~ 10의10제곱 까지의 제곱승을 구하라.
+
+		int a = 10;
+		long tot = 1;
+		for(int i=0; i<=10; i++) {	
+			System.out.println("10^" + String.format("%2d",i) +  " =" + String.format("%12d",tot) );
+			tot *= a;
+		}
+		
+		
+	}
+
+}
